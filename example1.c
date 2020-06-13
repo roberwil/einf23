663bf666601111ab17569bdfd1d2b67b16e80f33
Roberto Ribeiro <rober.will@hotmail.com>
2020-06-13 23:39:13 +0100

    modified:   example1.c

--- example1.c
+++ example1.c
@@ -6,9 +6,13 @@ int main(int argc, char const *argv[])
 {
     Read read = build_read();
     String str;
+    int number;
 
     str = read.string();
-    printf("%s", str);
+    printf("%s\n", str);
+
+    number = read.integer();
+    printf("%d\n", number)
 
     return 0;
 }
