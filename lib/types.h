9298d437ff801c594dfdf1627ed1e7e91ce44009
Roberto Ribeiro <rober.will@hotmail.com>
2020-06-28 01:48:11 +0100

    new file:   lib/array.c 	new file:   lib/array.h 	modified:   lib/types.h

--- lib/types.h
+++ lib/types.h
@@ -3,7 +3,9 @@
 
 #define ENTER_KEY   10
 #define TRUE        1
+#define true        1
 #define FALSE       0
+#define false       0
 #define INT        'i' 
 #define FLOAT      'f'
 #define END_STRING '\0'
@@ -12,9 +14,12 @@
 #define and         &&
 #define equals      ==
 #define ERR_NAN    "Invalid number!"
+
 #define Class       typedef struct
+#define Bool        unsigned short int
 
 typedef char*  String;
 typedef char** StringArray;
+
 
 #endif
