9dc3dbfe196c51a8d541c9155b44e241d8f79603
Roberto Ribeiro <rober.will@hotmail.com>
2020-08-01 23:56:11 +0100

    modified:   lib/file.c

--- lib/file.c
+++ lib/file.c
@@ -1,6 +1,17 @@
 #include "file.h"
 
-Bool each_line(OString path);
-int no_lines(OString path);
-OString read_lines(OString path);
-OString read_line(OString path);
+Bool each_line(OString path) {
+    return true;
+}
+
+int no_lines(OString path){
+    return 0;
+}
+
+OString read_lines(OString path){
+    return "result";
+}
+
+OString read_line(OString path) {
+    return "result";
+}
